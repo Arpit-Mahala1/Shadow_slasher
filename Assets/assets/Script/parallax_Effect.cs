76e0e2f956b141026146cff0fb68d1f15c43e31c
Arpit Mahala <arpitm39_soe@jnu.ac.in>
2025-02-19 04:03:48 +0530

    Animation_1

--- Assets/assets/Script/parallax_Effect.cs
+++ Assets/assets/Script/parallax_Effect.cs
@@ -1,18 +1,28 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class parallax_Effect : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Camera cam;
+    public Transform followPlayer;
+    Vector2 startingPosition;
+    float startingZ;
+    Vector2 carMovedSinceStart => (Vector2)cam.transform.position-startingPosition;
+    float zDistanceFromTarget => transform.position.z - followPlayer.transform.position.z;
+    float clippingPlane=> (cam.transform.position.z +(zDistanceFromTarget > 0 ? cam.farClipPlane: cam.nearClipPlane));
+    float parallaxFactor => Mathf.Abs(zDistanceFromTarget) / clippingPlane;
     void Start()
     {
-        
+        startingPosition = transform.position;
+        startingZ= transform.position.z;
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
-        
+        Vector2 newPosition = startingPosition + carMovedSinceStart * parallaxFactor;
+        transform.position =new Vector3(newPosition.x, newPosition.y, startingZ);
     }
 }
